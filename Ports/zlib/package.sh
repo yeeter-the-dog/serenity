6fb7211cdecb62267da41c83752431d62429d1c0
Anonymous <yeeter@yeeters.org>
2021-06-19 17:15:20 -0700

    Fix building Lagom on macOS12 beta

--- Ports/zlib/package.sh
+++ Ports/zlib/package.sh
@@ -5,11 +5,8 @@ useconfigure=true
 files="https://www.zlib.net/zlib-${version}.tar.gz zlib-${version}.tar.gz
 https://www.zlib.net/zlib-${version}.tar.gz.asc zlib-${version}.tar.gz.asc"
 
-auth_type="sig"
-auth_import_key="783FCD8E58BCAFBA"
-auth_opts="zlib-${version}.tar.gz.asc"
-
 configure() {
+    export CROSS_PREFIX=/usr/local/bin
     run ./configure
 }
 
